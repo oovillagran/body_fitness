fa89560c398e1e00237baa6c7e35c59bbb38b23c
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-29 11:19:38 -0500

    Add linters config

--- src/App.js
+++ src/App.js
@@ -5,47 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <a
+          className="App-link"
+          href="https://redux-toolkit.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux Toolkit
+        </a>
+        <a
+          className="App-link"
+          href="https://react-redux.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Redux
+        </a>
       </header>
     </div>
   );
